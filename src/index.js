529fd4f07069fc636e2e2790a9aa9a0f3d1aac93
duytadinh98@gmail.com <duytadinh98@gmail.com>
2025-09-06 17:49:12 +0700

    Learning 20250906

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ const books = [
     author: "Mel Robbins",
     title: "The Let Them Theory",
     img: "./images/book-1.jpg",
+    id: 1,
   },
   {
     author: "Joel Roy",
     title: "Let him cook",
     img: "./images/book-2.jpg",
+    id: 2,
   },
   {
     author: "Author Third Book",
     title: "The 3rd book Title",
     img: "./images/book-3.jpg",
+    id: 3,
   },
 ];
 
@@ -25,7 +28,7 @@ const BookList = () => {
     <section className="booklist">
       <EventExamples />
       {books.map((book) => {
-        return <Book {...book} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
@@ -36,11 +39,15 @@ const EventExamples = () => {
     console.log(e.target.value);
   };
   const handleButtonClick = () => {
-    alert("Handle button click");
+    console.log("Handle button click");
+  };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
   };
   return (
     <section>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <h2>Typical Form</h2>
         <input
           type="text"
@@ -48,19 +55,26 @@ const EventExamples = () => {
           onChange={handleFormInput}
           style={{ margin: "1rem 0" }}
         ></input>
+        <div>
+          <button type="submit">Submit Button</button>
+        </div>
+        <button onClick={handleButtonClick}>Click this area</button>
       </form>
-      <button onClick={handleButtonClick}>Click this area</button>
     </section>
   );
 };
 
 const Book = (props) => {
   const { author, title, img } = props;
+  const printSomething = () => {
+    alert(title);
+  };
   return (
     <article className="book">
       <img src={img} alt={title}></img>
       <p>Title: {title}</p>
       <p>Author: {author}</p>
+      <button onClick={printSomething}>Click here</button>
     </article>
   );
 };
