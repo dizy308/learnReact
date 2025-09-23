fa8a7b38d30732232b0ec1f5a3fdc18600f722b7
duytadinh98@gmail.com <duytadinh98@gmail.com>
2025-09-23 08:13:26 +0700

    Finished Project 01

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
+  const [people, setPeople] = useState(data)
+  const [dataState, setDataState] = useState(true)
+  const clearPeople = () => {
+    setPeople([])
+    setDataState(false)
+  }
+  const updateData = () => {
+    setPeople(data)
+    setDataState(true)
+  }
+
+  return <main>
+          <section className="container">
+            <h1>There are {people.length} people</h1>
+            {people && people.map((item)=>{
+              return <div className="person" key={item.id}>
+                        <img src={item.image} alt={item.name} />
+                        <h4>{item.name}</h4>
+                        <p>{item.age}</p>
+                        <br />
+                      </div>
+                    
+            })}
+            {dataState?<button onClick={clearPeople} className="btn">Clear people</button>:<button onClick={updateData} className="btn">Update people</button>}
+          </section>
+        </main>
+}
 export default App;
